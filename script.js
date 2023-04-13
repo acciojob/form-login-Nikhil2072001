058886236238dbe41f276caa867cef65ee46233f
Nikhil Choudhary <116430126+Nikhil2072001@users.noreply.github.com>
2023-04-13 16:27:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-var name = document.getElementById(name);
+var name = document.querySelector(#name);
 	var l= document.getElementById(lname);
 	var ans= name+" "+l;
 	alert(ans);
